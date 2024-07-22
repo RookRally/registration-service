78164651a3df5dfb1ff53ed3df5148802f7d37f9
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2024-07-22 16:07:07 +0200

    add cognito user pool

---
+++ lib/registration-service-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class RegistrationServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const userPool = new cognito.UserPool(this, "rookrally", {
+      signInCaseSensitive: false,
+      selfSignUpEnabled: true,
+    });
+  }
+}
---
+++ test/registration-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RegistrationService from '../lib/registration-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/registration-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RegistrationService.RegistrationServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
