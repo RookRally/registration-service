3656786ef30b44707c5ff70a68b7efbb661aadc6
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2024-07-25 11:31:14 +0200

    publish user signed up event to SNS topic

--- lambdas/postConfirmation/index.ts
+++ lambdas/postConfirmation/index.ts
@@ -1,3 +1,30 @@
-export function handler(event: any, context: any) {
+import { SNS } from "aws-sdk";
+
+const sns = new SNS();
+
+export async function handler(event: any, context: any) {
+  console.log(
+    `Post confirmation for user ${event.userName} with id ${event.request.userAttributes.sub}`,
+  );
+
+  const userSignedUpEvent = {
+    userId: event.request.userAttributes.sub,
+    username: event.userName,
+  };
+  const message = {
+    Message: JSON.stringify(userSignedUpEvent),
+    TopicArn: process.env.USER_SIGNED_UP_TOPIC_ARN,
+  };
+
+  try {
+    const result = await sns.publish(message).promise();
+    console.log(
+      `Message ${message.Message} sent to the topic ${message.TopicArn}`,
+    );
+    console.log("MessageID is " + result.MessageId);
+  } catch (error) {
+    console.error(error);
+  }
+
   return event;
 }
--- lib/registration-service-stack.ts
+++ lib/registration-service-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as cognito from "aws-cdk-lib/aws-cognito";
 import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
+import * as sns from "aws-cdk-lib/aws-sns";
 
 export class RegistrationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,13 +14,18 @@ export class RegistrationServiceStack extends cdk.Stack {
     });
     userPool.addClient("rookrallyClient");
 
+    const userSignedUpTopic = new sns.Topic(this, "userSignedUpTopic");
+
     const postConfirmationLambda = new lambda.NodejsFunction(
       this,
       "PostConfirmationLambda",
       {
         entry: "lambdas/postConfirmation/index.ts",
+        environment: { USER_SIGNED_UP_TOPIC_ARN: userSignedUpTopic.topicArn },
       },
     );
+
+    userSignedUpTopic.grantPublish(postConfirmationLambda);
 
     userPool.addTrigger(
       cognito.UserPoolOperation.POST_CONFIRMATION,
